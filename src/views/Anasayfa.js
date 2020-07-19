1fc26b47206087ee17d91a7d6e4508d2b8708367
yasin_enkaz@hotmail.com <yasindemir94>
2020-07-19 11:40:42 +0300

    status bar, üst bölge ve not bölümü ayarlandı

--- src/views/Anasayfa.js
+++ src/views/Anasayfa.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StatusBar} from 'react-native';
 import {observer} from 'mobx-react';
 
 import C from '../controllers/AnasayfaC';
@@ -9,6 +9,7 @@ import Resim from './Components/Resim';
 import telefonH from '../helper/telefonH';
 
 import {anasayfaS as S} from './stil';
+import temaH from '../helper/temaH';
 
 class Anasayfa extends React.Component {
   componentDidMount = C.cDMount;
@@ -17,25 +18,57 @@ class Anasayfa extends React.Component {
 
   ustBolge() {
     return (
-      <View
-        style={[
-          S.ustBolgeK,
-          {display: C.splashAktif ? 'none' : 'flex'},
-        ]}></View>
+      <View style={[S.ustBolgeK, {display: C.splashAktif ? 'none' : 'flex'}]}>
+        <Text style={S.bilgiY}>asdsdasdasdasd</Text>
+        <Text style={S.bilgiY}>asdsdasdasdasd</Text>
+        <Text style={S.bilgiY}>asdsdasdasdasd</Text>
+      </View>
+    );
+  }
+
+  notlar() {
+    return (
+      <View style={(S.notlarK, {display: C.splashAktif ? 'none' : 'flex'})}>
+        {this.not()}
+        {this.not()}
+        {this.not()}
+        {this.not()}
+      </View>
+    );
+  }
+
+  not() {
+    return (
+      <View style={S.notK}>
+        <Text>NOT</Text>
+      </View>
+    );
+  }
+
+  splash() {
+    return (
+      <View style={!C.splashAktif && S.logoK}>
+        <Resim
+          source={require('../../assets/logo.png')}
+          width={telefonH.W(C.splashAktif ? 60 : 20)}
+        />
+      </View>
     );
   }
 
   render() {
-    const sa = C.splashAktif;
     return (
-      <View style={[S.K, sa && S.K2]}>
-        <View style={!sa && S.logoK}>
-          <Resim
-            source={require('../../assets/logo.png')}
-            width={telefonH.W(C.splashAktif ? 60 : 20)}
-          />
-        </View>
+      <View style={[S.K, C.splashAktif && S.K2]}>
+        <StatusBar
+          backgroundColor={C.splashAktif ? 'transparent' : temaH.renkler.r1}
+          barStyle={'dark-content'}
+        />
+
         {this.ustBolge()}
+
+        {this.notlar()}
+
+        {this.splash()}
       </View>
     );
   }
