a01649d6454461f6e7ad7da8d9c82ee16b2c33fe
yasin_enkaz@hotmail.com <yasindemir94>
2020-07-22 03:20:33 +0300

    splash ayrıldı,notlar kaydırılabilir oldu

--- src/views/Anasayfa.js
+++ src/views/Anasayfa.js
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  StatusBar,
-  TouchableOpacity,
-  ScrollView,
-  FlatList,
-} from 'react-native';
+import {View, Text, StatusBar, TouchableOpacity, FlatList} from 'react-native';
 import {observer} from 'mobx-react';
 import {View as ViewA} from 'react-native-animatable';
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -19,6 +12,7 @@ import telefonH from '../helper/telefonH';
 
 import {anasayfaS as S} from './stil';
 import temaH from '../helper/temaH';
+import Splash from './Splash';
 
 class Anasayfa extends React.Component {
   componentDidMount = C.cDMount;
@@ -36,91 +30,143 @@ class Anasayfa extends React.Component {
   }
 
   notlar() {
+    const notlar = [
+      //SUNUCUDAN GELECEK
+      {
+        aciklama:
+          'Irure dolore voluptate voluptate dolor amet anim aliquip fugiat est Lorem in aliqua dolor.sdfsdfsdf',
+        gorseller: [
+          'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg',
+        ],
+        renk: 'r3',
+      },
+      {
+        aciklama:
+          'Irure dolore voluptate voluptate dolor amet anim aliquip fugiat est Lorem in aliqua dolor.',
+        gorseller: [
+          'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg',
+          'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg',
+          'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg',
+          'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg',
+        ],
+        renk: 'r2',
+      },
+      {
+        aciklama:
+          'Irure dolore voluptate voluptate dolor amet anim aliquip fugiat est Lorem in aliqua dolor.',
+        renk: 'r8',
+      },
+      {
+        aciklama:
+          'Irure dolore voluptate voluptate dolor amet anim aliquip fugiat est Lorem in aliqua dolor.',
+        renk: 'r7',
+      },
+      {
+        aciklama:
+          'Irure dolore voluptate voluptate dolor amet anim aliquip fugiat est Lorem in aliqua dolor.',
+        renk: 'r5',
+      },
+      {
+        aciklama:
+          'Irure dolore voluptate voluptate dolor amet anim aliquip fugiat est Lorem in aliqua dolor.',
+        renk: 'r6',
+      },
+      {
+        aciklama:
+          'Irure dolore voluptate voluptate dolor amet anim aliquip fugiat est Lorem in aliqua dolor.',
+        renk: 'r4',
+      },
+      {
+        aciklama:
+          'Irure dolore voluptate voluptate dolor amet anim aliquip fugiat est Lorem in aliqua dolor.',
+        renk: 'r1',
+      },
+    ];
+
     return (
-      <View
-        style={[
-          S.notlarK,
-          //{ display: C.splashAktif ? 'none' : 'flex' }
-        ]}>
-        {this.not()}
-        {this.not()}
-        {this.not()}
-        {this.not()}
+      <View style={S.notlarK}>
+        <FlatList
+          data={notlar}
+          extraData={notlar}
+          renderItem={(d) => this.not(d.item, d.index)}
+          //initialNumToRender={30}
+          //keyExtractor={(d, i) => (i.toString())}
+          //initialScrollIndex={5}
+          //ItemSeparatorComponent={() => <View style={{ backgroundColor: 'black', width: '75%', height: 2, alignSelf: 'center' }} />}
+        />
       </View>
     );
   }
 
-  not() {
-    const resimler = [
-      'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg',
-      'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg',
-      'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg',
-      'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg',
-    ];
-
-    const tekResim = resimler.length === 1;
+  not(d, i) {
+    const tekResim = d.gorseller && d.gorseller.length === 1;
 
     return (
       <ViewA
         animation={'bounceInRight'}
         delay={250}
+        //key={i}
         style={[S.notK /*{ paddingBottom: C.notButonlarKH }*/]}>
-        <View>
-          <Resim style={S.notResim} source={{uri: resimler[0]}} />
+        <View style={S.AK}>
+          <Resim
+            style={S.notResim}
+            source={{
+              uri:
+                'https://fujifilm-x.com/wp-content/uploads/2019/08/x-t3_sample-images02.jpg',
+            }}
+          />
 
-          <Text style={S.notAciklamaY}>
-            Lorem ipsum dolar sit amet Lorem ipsum dolar sit amet Lorem ipsum
-            dolar sit amet Lorem
-          </Text>
+          <Text style={S.notAciklamaY}>{d.aciklama}</Text>
         </View>
-
-        <View
-          style={S.notButonlarK}
-          //onLayout={d => C.notButonlarKH === 0 && C.set('notButonlarKH', d.nativeEvent.layout.height / 2.2)}
-        >
-          <TouchableOpacity
-            style={S.notIkon}
-            onPress={() => alert('test')}
-            activeOpacity={0.2}>
-            <AntDesign
-              name={'delete'}
-              color={temaH.renkler.r2}
-              size={telefonH.W(5.5)}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity style={S.notIkon}>
-            <AntDesign
-              name={'delete'}
-              color={temaH.renkler.r2}
-              size={telefonH.W(5.5)}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity style={S.notIkon}>
-            <AntDesign
-              name={'delete'}
-              color={temaH.renkler.r2}
-              size={telefonH.W(5.5)}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity style={S.notIkon}>
-            <AntDesign
-              name={'delete'}
-              color={temaH.renkler.r2}
-              size={telefonH.W(5.5)}
-            />
-          </TouchableOpacity>
-        </View>
+        {this.notButonlar(d, i)}
       </ViewA>
     );
   }
 
-  splash() {
+  notButonlar(d, i) {
     return (
-      <View style={!C.splashAktif && S.logoK}>
-        <Resim
-          source={require('../../assets/logo.png')}
-          width={telefonH.W(C.splashAktif ? 60 : 20)}
-        />
+      <View
+        style={[S.notButonlarK, {backgroundColor: temaH.notRenkleri[d.renk]}]}>
+        <TouchableOpacity
+          style={S.notIkon}
+          onPress={() => alert('test')}
+          activeOpacity={0.2}>
+          <AntDesign
+            name={'delete'}
+            color={temaH.renkler.r2}
+            size={telefonH.W(5.5)}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={S.notIkon}
+          onPress={() => alert('test')}
+          activeOpacity={0.2}>
+          <AntDesign
+            name={'delete'}
+            color={temaH.renkler.r2}
+            size={telefonH.W(5.5)}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={S.notIkon}
+          onPress={() => alert('test')}
+          activeOpacity={0.2}>
+          <AntDesign
+            name={'delete'}
+            color={temaH.renkler.r2}
+            size={telefonH.W(5.5)}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={S.notIkon}
+          onPress={() => alert('test')}
+          activeOpacity={0.2}>
+          <AntDesign
+            name={'delete'}
+            color={temaH.renkler.r2}
+            size={telefonH.W(5.5)}
+          />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -137,7 +183,7 @@ class Anasayfa extends React.Component {
 
         {!C.splashAktif && this.notlar()}
 
-        {this.splash()}
+        <Splash />
       </View>
     );
   }
