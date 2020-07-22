9c3236cccfc0eba560e2d3083982539e49b55cd1
yasin_enkaz@hotmail.com <yasindemir94>
2020-07-22 19:16:47 +0300

    ++

--- src/views/stil.js
+++ src/views/stil.js
@@ -21,61 +21,100 @@ export const anasayfaS = StyleSheet.create({
   AK: {
     flexDirection: 'row',
   },
-
-  ustBolgeK: {
-    paddingLeft: W(20) + H(2),
-    minHeight: W(20) + H(4),
-    justifyContent: 'center',
-    backgroundColor: temaH.renkler.r1,
-    //margin: W(2),
-    //borderRadius: W(2),
-  },
-  bilgiY: {
-    fontWeight: 'bold',
-    fontSize: 15,
-    textAlign: 'right',
-    paddingRight: W(3),
-    color: temaH.renkler.r2,
-  },
-
-  notlarK: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: 'red',
-  },
-  notK: {
-    backgroundColor: temaH.renkler.r1,
-    marginTop: H(1.2),
-    marginBottom: H(1.2),
-    padding: W(2),
-  },
-  notAciklamaY: {
-    fontWeight: 'bold',
-    color: temaH.renkler.r2,
-    flex: 1,
-    paddingLeft: W(3),
-    textAlignVertical: 'center',
-  },
-  notResim: {borderRadius: 3},
-  notButonlarK: {
-    borderBottomWidth: 0,
-    position: 'absolute',
-    bottom: -10,
-    right: 10,
-    borderRadius: 3,
-    backgroundColor: temaH.renkler.r1,
-    flexDirection: 'row',
-    paddingBottom: 3,
-  },
-  notIkon: {
-    marginLeft: 3,
-    marginRight: 3,
-  },
 });
 export const splashS = StyleSheet.create({
   K: {
+    //splash ekranı container
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  K2: {
+    //anasayfa için splash ekranı container
     position: 'absolute',
-    left: W(4),
+    left: W(2),
     top: H(2),
+  },
+});
+
+export const anasayfaNotS = StyleSheet.create({
+  notlarK: {
+    //tüm notların içinde bulunduğu View
+    flex: 1,
+  },
+  notK: {
+    //her bir not için View
+    backgroundColor: R.r1,
+    marginTop: H(1.2),
+    marginBottom: H(1.2),
+    padding: W(2),
+    paddingBottom: W(9),
+  },
+
+  notButonlarK: {
+    //notlardaki açılır butonlar alanı
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: -10,
+    right: 10,
+    backgroundColor: R.r1,
+    borderRadius: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    backgroundColor: R.r1,
+    borderWidth: 2,
+    borderBottomWidth: 0,
+    borderColor: 'white',
+  },
+  notButonlarAK: {flexDirection: 'row'},
+  butonlarAcKapaButon: {
+    alignSelf: 'center',
+  },
+  notButonK: {
+    marginRight: 3,
+    marginLeft: 3,
+  },
+
+  notRenkSecK: {
+    borderWidth: 1,
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: -W(2),
+    left: 10,
+    maxWidth: W(30),
+    backgroundColor: R.r1,
+    borderRadius: 2,
+    borderWidth: 2,
+    borderBottomWidth: 0,
+    borderColor: 'white',
+    paddingTop: 3,
+    paddingBottom: 3,
+  },
+  notRenk: {
+    width: W(6),
+    height: W(6),
+    borderRadius: W(3),
+    marginRight: 5,
+    marginLeft: 5,
+  },
+});
+
+export const anasayfaUstBolgeS = StyleSheet.create({
+  K: {
+    minHeight: W(20) + H(4),
+    width: W(100),
+    backgroundColor: R.r1,
+    paddingLeft: W(25),
+    paddingRight: 15,
+    justifyContent: 'center',
+  },
+  ustAlanY: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: R.r2,
+    textAlign: 'right',
   },
 });
