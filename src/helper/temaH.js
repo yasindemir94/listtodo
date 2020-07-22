2f7dd0bdac1fd7d8da2628beddff6af10c129398
yasin_enkaz@hotmail.com <yasindemir94>
2020-07-22 03:19:25 +0300

    yeni renkler eklendi

--- src/helper/temaH.js
+++ src/helper/temaH.js
@@ -5,10 +5,24 @@ class temaH {
     r1: '#e89d43',
     r2: '#0d1030',
   };
+
+  notRenkleri = {
+    r1: '#eb4034',
+    r2: '#ff7e75',
+    r3: '#ffbc75',
+    r4: '#ffff75',
+    r5: '#d4edbb',
+    r6: '#b9fa78',
+    r7: '#4bebd3',
+    r8: '#9b4beb',
+  };
+
   set = (k, v) => (this[k] = v);
 }
 
 decorate(temaH, {
+  renkler: observable,
+  notRenkleri: observable,
   set: action,
 });
 
