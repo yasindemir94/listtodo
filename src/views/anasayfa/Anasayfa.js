b3960d31f45e5f85e380a501359b00ef5be7e719
yasin_enkaz@hotmail.com <yasindemir94>
2020-07-26 09:51:48 +0300

    ++

--- src/views/anasayfa/Anasayfa.js
+++ src/views/anasayfa/Anasayfa.js
@@ -1,45 +1,44 @@
 import React from 'react';
-import { View } from 'react-native';
-import { observer } from 'mobx-react';
+import {View} from 'react-native';
+import {observer} from 'mobx-react';
 
 import Arkaplan from '../../components/Arkaplan';
 
-import C from '../../controllers/anasayfa/anasayfaC';
+import C from '../../controllers/anasayfa/AnasayfaC';
 import splashC from '../../controllers/splashC';
 
-import { anasayfaS as S } from '../stil';
-
+import {anasayfaS as S} from '../stil';
 
 import UstBolge from './UstBolge';
 import Notlar from './Notlar';
 import Splash from '../Splash';
 
-
 class Anasayfa extends React.Component {
-    componentDidMount = C.cDMount;
-    componentDidUpdate = C.cDUpdate;
-    componentWillUnmount = C.cWUnmount;
+  componentDidMount = C.cDMount;
+  componentDidUpdate = C.cDUpdate;
+  componentWillUnmount = C.cWUnmount;
 
+  render() {
+    const durum = splashC.durum;
 
-    render() {
-        const durum = splashC.durum;
+    return (
+      <View style={[S.K, durum && S.K2]}>
+        <Arkaplan
+          source={require('../../../assets/back2.jpg')}
+          opacity={durum === 0 ? 0 : 0.8}
+        />
 
-        return (
-            <View style={[S.K, durum && S.K2]}>
-                <Arkaplan source={require('../../../assets/back2.jpg')} opacity={durum === 0 ? 0 : 0.8} />
+        {durum === 3 && (
+          <>
+            <UstBolge />
+            <Notlar />
+          </>
+        )}
 
-                {
-                    durum === 3 &&
-                    <>
-                        <UstBolge />
-                        <Notlar />
-                    </>
-                }
-
-                <Splash />
-            </View>
-        );
-    }
+        <Splash />
+      </View>
+    );
+  }
 }
 
 export default observer(Anasayfa);
