f0c819358f652e19cd0c1c200180ceb8e85f9087
yasin_enkaz@hotmail.com <yasindemir94>
2020-08-09 16:08:56 +0300

    ++

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import { AppRegistry, UIManager, Platform } from 'react-native';
 import App from './src/views/App';
-import telefonH from './src/helper/tlfnH';
+import tlfnH from './src/helper/tlfnH';
 
 import 'react-native-console-time-polyfill';
 
-if (telefonH.and) UIManager.setLayoutAnimationEnabledExperimental(true);
+if (tlfnH.and) UIManager.setLayoutAnimationEnabledExperimental(true);
 
 //console.disableYellowBox = true;
 
